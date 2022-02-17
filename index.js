c63769665a0a6ee98d1bec22b5c91dc8a60d6e11
Ravinand25 <97322924+Ravinand25@users.noreply.github.com>
2022-02-17 23:00:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 // If Player 1 Wins
 if (randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Ravi Wins!";
 }
 else if (randomNumber2> randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "Sulu Wins! 🚩";
 }
 else{
     document.querySelector("h1").innerHTML = "Draw!";
